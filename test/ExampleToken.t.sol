887fc3347865475ea07a51fcca440e796ea89ccb
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-06-14 15:00:05 +0900

    test

---
+++ test/ExampleToken.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity =0.8.9;
+
+import "forge-std/src/Test.sol";
+import "../contracts/ExampleToken.sol";
+
+
+contract ExampleTokenTest is Test {
+	ExampleToken exampleToken;
+
+	function setUp() public {
+		exampleToken = new ExampleToken();
+		exampleToken.initialize();
+	}
+
+	function testGetName() public {
+        assertTrue(exampleToken.name() == "token");
+    }
+
+	function testGetSymbol() public {
+        assertTrue(exampleToken.symbol() == "TOKEN");
+    }
+}
