f5b3675322a35bc414803d3e969316ba87b38df5
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-07-08 15:53:28 +0900

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import '@nomicfoundation/hardhat-toolbox'
-// Import '@typechain/hardhat'
-// import '@nomiclabs/hardhat-ethers'
-// import '@nomiclabs/hardhat-waffle'
-// import '@nomiclabs/hardhat-etherscan'
 import '@openzeppelin/hardhat-upgrades'
-
 import '@nomicfoundation/hardhat-chai-matchers'
 import * as dotenv from 'dotenv'
 
